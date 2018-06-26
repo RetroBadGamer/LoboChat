d0cb162207bec33d2ceb959f47e644ce6740eb0c
RetroBadGamer <lobatox4@gmail.com>
2018-06-26 00:26:43 -0300

    cambiando la databaseURL

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@
   
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyC5q5AkIyF9qyOSsGLGnTgx4JM-HJECsjU",
-    authDomain: "ninja-chat-77ab3.firebaseapp.com",
-    databaseURL: "https://ninja-chat-77ab3.firebaseio.com",
-    projectId: "ninja-chat-77ab3",
-    storageBucket: "ninja-chat-77ab3.appspot.com",
-    messagingSenderId: "367090526976"
+    apiKey: "AIzaSyAqzIWakGNMv9Qpx4-XJLD_g0UylQ0iCf0",
+    authDomain: "lobochat-app.firebaseapp.com",
+    databaseURL: "https://lobochat-app.firebaseio.com",
+    projectId: "lobochat-app",
+    storageBucket: "lobochat-app.appspot.com",
+    messagingSenderId: "1066676069084"
   };
   const firebaseApp = firebase.initializeApp(config);
   firebaseApp.firestore().settings({ timestampsInSnapshots: true })
